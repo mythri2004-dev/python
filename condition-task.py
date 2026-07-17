829e1bfc0e3e2c700c675fc0df9b5849cde707de
mythri2004-dev <mythrimula64@gmail.com>
2026-07-17 15:07:13 +0530

    Add files via upload

---
+++ condition-task.py
@@ -0,0 +1,107 @@
+#task
+#voteing
+'''age =int(input("enter a number"))
+if age >= 18:
+    print("eligiblible for vote")
+else:
+    print("not elgibile for vote")'''
+#even or add 
+'''num = int(input("enter a number: "))
+if num %2 == 0:
+    print("it is even")
+else:
+    print("it is odd")''' 
+
+#leap year
+'''year=int(input("enter the number"))
+if year %4==0:
+    print("it is a leap year")
+else:
+    print("it is not a leap year")'''  
+
+
+#guest code
+'''name =input("enter a name")
+if name =="pooja":
+    print("welcome , name")
+else:
+     print("welcome to guest")'''
+
+'''name =input("enter a name")
+if name =="mythri" or "pooja" or "ramya" or "geetha" or "prathap":
+     print("welcome",name)
+else:
+        print("welcome guest")'''
+
+
+'''names=["mythri" , "pooja" ,"ramya" , "geetha" ,"prathap"]
+a=input("enter a name") . lower() 
+if a in names:
+     print("welcome",a)
+else:
+        print("welcome")'''
+
+#vowels
+'''vowels=["a","e","i","o","u"]
+a=input("enter a letter")
+if a in vowels:
+    print("it is a vowel")
+else:
+    print("it is constant")'''
+
+#social-media login
+username=input("enter the username")
+password=int(input("enter the password"))
+if username=="pooja":
+    if password==1234:
+        print("login successful")
+else:
+    print("invalid creditials") 
+    
+username=input("enter the username")
+password=int(input("enter the password"))
+if username=="pooja":
+    if password==1234:
+        print("login successful")
+    else:
+        print("incorrect password") 
+else:
+    print("invalid username") 
+    
+
+username=input("enter the username")
+password=int(input("enter the password"))
+if username=="pooja" and password=="pooja@1234": 
+    if password==1234:
+        print("login successful")
+else:
+    print("invalid creditials")
+
+
+
+username=input("enter the username")
+password=int(input("enter the password"))
+if username=="pooja" and password=="1234": 
+    if password==1234:
+        print("login successful")
+else:
+    print("invalid creditials")
+
+#marks
+age = int(input("Enter your age: "))
+marks = int(input("Enter your marks: "))
+attendance = int(input("Enter your attendance: "))
+
+if age >= 18:
+    print("Eligible for vote")
+
+if marks >= 80:
+    print("Eligible for scholarship")
+
+if attendance >= 70:
+    print("Allowed to write exam")
+    
+
+
+    
+
