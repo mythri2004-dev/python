13f0549e9eb9c0b8d45db55e620bb0d488a8ee06
mythri2004-dev <mythrimula64@gmail.com>
2026-07-17 11:07:35 +0530

    Add files via upload

---
+++ run time input,swapping.py
@@ -0,0 +1,134 @@
+'''a=2
+b=4
+print(a+b)'''
+
+
+'''a=6
+b=8
+c=a+b
+print(c)'''
+
+#print(10+10)
+
+
+#run_time input()
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+print(a+b)'''
+
+'''a=float(input("enter the a value"))
+b=float(input("enter the b value"))
+print(a+b)'''
+
+'''a=str(input("data1"))
+b=str(input("data2"))
+print(a+b)'''
+
+
+'''a=input("data1")
+b=input("data2")
+print(a+b)''' 
+
+'''fname=input("first name")
+lname=input("last name")
+print((fname+" "+lname).title())'''  
+
+'''a=complex(input("enter the a vaalue"))
+b=complex(input("enter the b value"))
+print(a+b)'''
+
+'''a=bool(input("enter the a value"))
+b=bool(input("enter the b value"))
+c=bool(input("c value"))
+print(a+b+c)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)''' 
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input(choose the option
+                  1.add
+                  2.sub
+                  3.mul))
+print(a+b)
+print(a-b)
+print(a*b)''' 
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=input("choose the option add sub mul")
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+'''a=input()
+print(a)'''
+
+#task
+#swapping of two variables
+#method-1 
+'''a=10
+b=20
+a,b=b,a
+print("a value is",a)
+print("b value is",b)''' 
+
+#method 2
+'''a=10
+b=20
+temp=a
+a=b
+b=temp 
+print("a value is",a)
+print("b value is",b)'''
+
+#method-3
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("a value is",a)
+print("b value is",b)'''
+
+#method-4
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%d,b=%d" %(a,b))'''
+
+'''a=float(input("a value"))
+b=float(input("b value"))
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%2f,b=%2f" %(a,b))'''
+
+
+'''a=input("data1")
+b=input("data2")
+temp=a
+a=b
+b=temp
+print("after swapping a=%s,b=%s" %(a,b))'''
+
+
+
+
+
+
+
+
+
