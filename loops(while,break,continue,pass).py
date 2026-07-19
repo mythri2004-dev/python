8291d58c4a0486156fd85563d533b853d9ae2843
mythri2004-dev <mythrimula64@gmail.com>
2026-07-19 15:07:01 +0530

    Add files via upload

---
+++ loops(while,break,continue,pass).py
@@ -0,0 +1,150 @@
+#while loop
+'''a=10
+while a>1:
+    print(a)'''
+
+'''a=10
+while a>1:
+    print(a)
+    a=a-1'''
+
+'''a=10
+while a>=1:
+    print(a)
+    a=a-1'''
+    
+'''a=10
+while a>1:
+    a=a-1
+    print(a)''' 
+        
+'''a=20
+    while a>5:
+    a=a-1
+    print(a)'''
+
+'''a=20
+while a>5:
+    a=a-1
+print(a)'''
+
+'''a=30
+while a>2:
+    print(a)
+    a+=1''' 
+    
+#voteing
+'''while True:
+    age =int(input("enter the age"))
+    if age>=18:
+       print("eligible for vote")
+    else:
+        print("eligible for vote")'''
+        
+#range()
+#start-stop-step
+'''for i in range(10): 
+    print(i)'''
+
+'''for i in range(5,15): 
+    print(i)'''
+
+'''for i in range(30,45): 
+    print(i,end=" ,")'''
+
+'''for i in range(2,20,2): 
+    print(i,end=" ,")'''
+
+'''for i in range(5,50,5): 
+    print(i,end=" ,")'''
+
+#grade code
+'''while True:
+    marks=int(input("enter the marks"))
+    if marks in range(91,101):
+        print("Grade-A")
+    elif marks in range(81,91):
+        print("Grade-B") 
+    elif marks in range(71,81):
+        print("Grade-C")
+    elif marks in range(50,71):
+        print("Grade-D")
+    else:
+         print("Fail,studdy well........")'''
+
+#break
+a=20
+'''while a>5:
+    print(a)
+    a=a-1
+    if a==10:
+       break'''
+'''a=20
+while a>5:
+    a=a-1
+    if a==10:
+       break
+    print(a)'''
+'''a=30
+while a>2:
+    a=a-1
+    if a==20:
+       break
+    print(a)'''
+
+'''for i in range(40,65):
+    if i==55:
+        break
+    print(i)'''
+
+'''a="python"
+if a=="h":
+    break
+print(a)''' #error
+
+'''a="python"
+for i in a:
+    if i=="h": 
+        break
+    print(i)'''
+
+#continue
+'''a=15
+while a>3:
+    a=a-1
+    if a==11:
+        continue
+    print(a)'''
+
+'''a=15
+while a>3:
+    print(a)
+    a=a-1
+    if a==11:
+        continue'''
+
+'''for i in range(18):
+    if i==14:
+        continue
+    print(i)''' 
+
+'''a="python"
+for i in a:
+    if i=="t":
+        continue
+    print(i)'''
+
+#pass
+'''a=12
+while a>4:
+    print(a)
+    a=a-1
+    if a==10:
+        pass'''
+
+'''for i in range(25):
+    if i==10:
+        pass
+    print(i)''' 
+    
+
