9deaaadb1a4825138801f48534e0182ceb09252a
mythri2004-dev <mythrimula64@gmail.com>
2026-07-19 10:04:20 +0530

    Add files via upload

---
+++ loops(for).py
@@ -0,0 +1,140 @@
+#task by using if elif else
+#Bakery
+'''a=int(input("enter the cake price"))
+if a==1200:
+    print("redvelvet cake")
+elif a==1000:
+    print("almond cake")
+elif a==800:
+    print("chocolate cake")
+elif a==600:
+    print("butterscotch cake")
+else:
+    print("sorry cake is not available")'''
+
+#task by using if elif 
+#Pizza
+'''a=input("enter the pizza name")
+if a=="bbq pizza":
+    print(1000)
+elif a=="crispy chicken pizza":
+    print(800)
+elif a=="panneer pizza":
+    print(600)
+elif a=="corn pizza":
+    print(400)
+elif a=="frenchfries & coke":
+    print(200)'''
+
+#loops
+#for,while,range,break,continue,pass
+#for loop()
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(a)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=" ,")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=(4,5,6,7,8,9)
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a={4,5,6,7,8,9}
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''d={"year":2026,"month":"july","date":10}
+for i in d:
+    print(i)
+for i in d.keys():
+    print(i)
+    print(type(d))
+    print(type(i))
+for i in d.keys():
+    print(type(d))
+    print(type(i))
+for i in d.values():
+    print(type(d))
+    print(type(i))
+for i in d.items():
+    print(type(d))
+    print(type(i))'''
+
+'''a="codegnan"
+for i in a:
+    print(i)'''
+
+'''a=[3.4,5.6]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=["python","java","html","css"]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+
+'''a=[5+9j,2+10j]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=[True,False]
+for i in a:
+     print(i)
+print(type(a))
+print(type(i))'''
+
+#task
+a=["apple","banana","mango"]
+#["APPLE","BANANA","MANGO"] 
+#print (fruits.upper())
+
+'''b=str(a)
+print(b.upper())'''
+
+'''b=[]
+for i in a:
+    b.append(i.upper())
+print(b)'''
+
+a=[10,20,30,40,50,"code"]
+#["10,20,30,40,50,"code","c","o","d","e"]
+'''a.extend("code")
+print(a)'''
+
+
+
+
+
+
+
+
+    
+    
