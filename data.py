2be945d12bd1a4169bc670fe106fab3ddc2aba85
mythri2004-dev <mythrimula64@gmail.com>
2026-07-28 11:55:31 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,14 @@
+'''import  mymodule
+mymodule.greetings("pooja")'''
+
+#import mymodule
+
+
+
+#import mymodule 
+
+'''import mymodule
+a = mymodule.details["names"]
+b = mymodule.details["idnos"]
+print(a,b)
+print(b)'''  
